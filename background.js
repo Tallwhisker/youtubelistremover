e3f5bbe28f0a60f67c3bcbdbfe227e9a10f40516
Kevin <164023995+Tallwhisker@users.noreply.github.com>
2026-02-12 15:11:17 +0100

    Rewrote main script

--- background.js
+++ background.js
@@ -1,27 +1,26 @@
-function getUrl() {
-    browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
-        updateTab(tabs[0].url); //There should ever only be 1 result from current & active.
+const pattern = "https://www.youtube.com/watch?v=*&list*"
+
+function updateTab() {
+    //Get current tab
+    browser.tabs.query({url: pattern, active: true}).then((tabs) => {
+        if(tabs.length > 0 && tabs[0].url)
+        {
+            let url = tabs[0].url;
+            const listIndex = url.search("&list=");
+            let newUrl = url.substring(0, listIndex);
+
+            browser.tabs.update({
+                url: newUrl
+            });
+            console.log("Trimmed YouTube URL of list & refreshed current tab.")
+        }
+        else
+        {
+            console.log("Did not find any list to trim.");
+            return;
+        };
     }, console.error)
 };
 
-function updateTab(url) {
-    const listIndex = url.search("&list=");
-    const youtubeCheck = url.search("youtube.com/watch");
-
-    //Don't run if there's no list or not on youtube.
-    if (youtubeCheck === -1) {
-        console.log("Not watching a youtube video.");
-        return;
-    } else if (listIndex == -1) {
-        console.log("Did not find a list to remove.");
-        return;
-    };
-
-    let newUrl = url.substring(0, listIndex);
-    browser.tabs.update({
-        url: newUrl
-    });
-    console.log("Removed the list and any following parameters.");
-};
-
-browser.browserAction.onClicked.addListener(getUrl);
+// Button for running manual URL trimmer
+browser.browserAction.onClicked.addListener(updateTab);
